6994c4fc4d85a34f54d33cf5086a1f52e862a158
Hao Lin <hlin368@wisc.edu>
2024-03-18 19:05:40 -0500

    redirect a short url to google slides of workshop 2

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
             year: "numeric", month: "short", day: "2-digit"
         }),
     },
+
+    async redirects() {
+        return [
+            {
+                source: '/slide-1',
+                destination: 'https://docs.google.com/presentation/d/18qZpsj2KUSb_I5RkAqmgjazQjBmAILwUdlUgYjVNGtM/edit?usp=sharing',
+                permanent: false,
+            }
+        ]
+    },
 };
